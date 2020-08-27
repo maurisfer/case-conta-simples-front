e1bdf6872e9526089e2cd6fe9afe72877bef5231
rodrigobostrom <rodrigo_bst@poli.ufrj.br>
2020-08-27 19:16:23 -0300

    Acrescentando Rota Not Found

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import Login from './pages/login/Login';
-import Home from './pages/home/home'
+import Home from './pages/home/home';
+import NotFound from './pages/404/404';
 import Criarcartao from './pages/cadastrar cartão/cadastrarcartao.js'
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
         <Route path="/home" component={Home}></Route>
         <Route path="/login" component={Login}></Route>
         <Route path="/cadastrarcartao" component={Criarcartao}></Route>
+        <Route path="*" component={NotFound}></Route>
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
