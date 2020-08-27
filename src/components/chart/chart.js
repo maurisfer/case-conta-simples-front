e5afb9dec78248b8c2cec17013477a46833631a4
fernandocarelle <fern@ndo.codes>
2020-08-26 22:27:15 -0300

    correcao de erros

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -26,7 +26,6 @@ const Grafico = () => {
       datasets: [
         {
           label: 'Entrada',
-<<<<<<< HEAD
           data: [0, 10, 500, 20, 1000, 550, 3000, 200, 100, 5000, 110],
           backgroundColor: ['rgba(0, 190, 62, 0.2)'],
           borderColor: ['rgba(0, 190, 62, 0.4)'],
@@ -42,39 +41,6 @@ const Grafico = () => {
       ],
     });
   };
-=======
-<<<<<<< HEAD
-          data: [0, 1000, 550, 3000, 200], //Irá consumir backend
-=======
-          data: [0,10, 500, 20, 1000, 550, 3000, 200, 100, 5000, 110 ], // Irá consumir do backend
->>>>>>> 3b956335ba7ad3f0679799db1b6294c3f957e7f5
-          backgroundColor: [
-            'rgba(0, 190, 62, 0.2)'
-          ],
-          borderColor: [
-            'rgba(0, 190, 62, 0.4)'
-          ],
-          borderWidth: 3
-        },
-        {
-          label: 'Entrada',
-<<<<<<< HEAD
-          data: [0, 1500, 350, 3500, 250], // Irá consumir o backend
-=======
-          data: [0,2000, 3000, 3000, 2000, 1000, 350, 3500, 250, 100, 200], //Irá consumir do backend
->>>>>>> 3b956335ba7ad3f0679799db1b6294c3f957e7f5
-          backgroundColor: [
-            'rgba(412, 90, 62, 0.2)'
-          ],
-          borderColor: [
-            'rgba(412, 90, 62, 0.4)'
-          ],
-          borderWidth: 3
-        }
-      ]
-    })
-  }
->>>>>>> 0cd9fc3db5acefc4dd48084da0e5724742ba91fa
 
   useEffect(() => {
     chart();
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -10,7 +10,6 @@ function Login() {
       <>
         <GlobalStyle />
         <Container>
-<<<<<<< HEAD
           <img
             id="imglogin"
             src={ImgLogin}
@@ -39,29 +38,36 @@ function Login() {
               <a href="/">Ainda não sou cliente {'>'}</a>
             </form>
           </div>
-=======
-          <img id="imglogin" src={ImgLogin} alt="Ilustração de mulher com cartão" />
-            <div id="loginform">
-              <form>
-                <img id="imglogo" src={Img} alt="Logo da conta simples"></img>
-                <h4>Preencha os campos abaixo para fazer login </h4>
-<<<<<<< HEAD
-                <label htmlFor="cpf" className="labellogin"> CNPJ: <br/> {/* Será CNPJ */}
-                  <input placeholder="Insira seu CPF" type='text' id="cpf"/>
-=======
-                <label htmlFor="cnpj" className="labellogin"> CPF: <br/>
-                  <input placeholder="Insira seu CPF" type='text' id="cnpj"/>
->>>>>>> 3b956335ba7ad3f0679799db1b6294c3f957e7f5
-                </label>
-                <label htmlFor="senha" className="labellogin"> Senha: <br/> {/* Irá fazer um post pro backend para validação */}
-                  <input placeholder="Insira sua senha" type='password' id="senha"/>
-                </label>
-                <input value="LOGIN" type= "submit" />
-                <a href="/">Esqueci minha senha {">"}</a> <br/>
-                <a href="/">Ainda não sou cliente {">"}</a> {/* Irá direcionar para um formulário de cadastro */}
-              </form>
-            </div>
->>>>>>> 0cd9fc3db5acefc4dd48084da0e5724742ba91fa
+          <img
+            id="imglogin"
+            src={ImgLogin}
+            alt="Ilustração de mulher com cartão"
+          />
+          <div id="loginform">
+            <form>
+              <img id="imglogo" src={Img} alt="Logo da conta simples" />
+              <h4>Preencha os campos abaixo para fazer login </h4>
+              <label htmlFor="cnpj" className="labellogin">
+                {' '}
+                CPF: <br />
+                <input placeholder="Insira seu CPF" type="text" id="cnpj" />
+              </label>
+              <label htmlFor="senha" className="labellogin">
+                {' '}
+                Senha: <br />{' '}
+                {/* Irá fazer um post pro backend para validação */}
+                <input
+                  placeholder="Insira sua senha"
+                  type="password"
+                  id="senha"
+                />
+              </label>
+              <input value="LOGIN" type="submit" />
+              <a href="/">Esqueci minha senha {'>'}</a> <br />
+              <a href="/">Ainda não sou cliente {'>'}</a>{' '}
+              {/* Irá direcionar para um formulário de cadastro */}
+            </form>
+          </div>
         </Container>
       </>
     </div>
