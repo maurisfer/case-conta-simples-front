f49db101e9b654d144c73bfe5c7e04855d52852e
leilaguimaraes <leilabeatriz.94@gmail.com>
2020-08-27 16:20:10 -0300

    criando pagina de cadastro

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import Login from './pages/login/Login';
 import Home from './pages/home/home'
+import Criarcartao from './pages/cadastrar cartão/cadastrarcartao.js'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
         <Route path="/home" component={Home}></Route>
-        <Route path="/Login" component={Login}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/cadastrarcartao" component={Criarcartao}></Route>
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/cadastrar cartão/cadastrarcartao.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Container } from './styles';
+import GlobalStyle from '../../assets/styles/global';
+import Imagemcartao from '../../assets/images/cadastrarcartao.svg'
+import Img from '../../assets/images/logo 01 mobile.svg';
+
+function Criarcartao() {
+  return (
+    <div className="Criarcartao">
+      <>
+        <GlobalStyle />
+        <Container>
+
+          <img
+            id="imglogin"
+            src={Imagemcartao}
+            alt="Ilustração de mulher com cartão"
+          />
+          <div id="loginform">
+            <form>
+              <img id="imglogo" src={Img} alt="Logo da conta simples" />
+              <h4>Preencha para cadastrar um novo cartão </h4>
+              <label htmlFor="nomecartao" className="labellogin">
+                {' '}
+                Nome do Cartão: <br />
+                <input placeholder="Insira o nome do cartão" type="text" id="nomecartao" />
+              </label>
+              <label htmlFor="senha" className="labellogin">
+                {' '}
+                Senha: <br />{' '}
+                {/* Irá fazer um post pro backend para validação */}
+                <input
+                  placeholder="Insira sua senha"
+                  type="password"
+                  id="senha"
+                />
+              </label>
+
+
+              <input value="CADASTRAR" type="submit" />
+
+            </form>
+          </div>
+        </Container>
+      </>
+    </div>
+  );
+}
+
+export default Criarcartao;
