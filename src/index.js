e961b8273d474c5ee463ccb17464d7acf2834514
leilaguimaraes <leilabeatriz.94@gmail.com>
2020-08-30 18:20:53 -0300

    criando a página de fazer cartões

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Login from './pages/login/Login';
 import Home from './pages/home/home';
 import NotFound from './pages/404/404';
 import Criarcartao from './pages/novo-cartão/cadastrarcartao'
+import Criarconta from './pages/criarconta/Criarconta'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.render(
         <Route path="/home" component={Home}></Route>
         <Route path="/login" component={Login}></Route>
         <Route path="/cadastrarcartao" component={Criarcartao}></Route>
+        <Route path="/criarconta" component={Criarconta}></Route>
         <Route path="*" component={NotFound}></Route>
       </Switch>
     </BrowserRouter>
